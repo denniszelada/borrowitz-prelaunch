594f0fced3eb5df34707bb28676319d658bd9928
Dennis Zelada <denniszelada@gmail.com>
2013-03-07 23:28:49 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BorrowitzPrelaunch::Application.config.secret_token = '80a378deef255afde33eb152f0ad61c122f3207ff13f761e99e05d88ee0b9f577ffcc5fb95c6b027f936ec10d3478afc73c5ccc90b20b1e5ea1a9ffa86a609c4'
